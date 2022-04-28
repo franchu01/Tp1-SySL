a81dfa5296559d6e24ed3ea02c89206a67bd030d
LucianoBauso <83524525+LucianoBauso@users.noreply.github.com>
2022-04-28 19:21:15 -0300

    Update Tp1.c

--- Tp1.c
+++ Tp1.c
@@ -6,9 +6,9 @@
 int tamanoCadena (char c[])
 {
     int contador = 0;
-    for(int i=0;c[i]!='\0';i++)
+    for(int i=0; c[i]!='\0'; i++)
         contador++;
-    return contador-1;
+    return contador;
 }
 
 
@@ -19,9 +19,9 @@ int caracterValido(const char cad)
     return cad >= '0' && cad <= '9';
 }
 
-int cadenaANumero(const char *cad, int tam)
+int cadenaANumero(char cad[])
 {
-    int i = tam - 1;
+    int i = tamanoCadena(cad) - 1;
     int entero = 0;
     int pos = 1;
     while(i >= 0)
@@ -32,7 +32,6 @@ int cadenaANumero(const char *cad, int tam)
             pos = pos*10;
             i--;
         }
-        pos = 1;
         i--;
     }
     return entero;
@@ -43,7 +42,8 @@ int cadenaANumero(const char *cad, int tam)
 void cadenaAMayusculas(char cad[])
 {
     char aux;
-    for(int i=0; cad[i] != '\n'; i++)
+    int i=0;
+    for(i; cad[i] != '\0'; i++)
     {
         if(cad[i] >= 97 && cad[i] <= 122)
             cad[i]=cad[i]-32;
@@ -63,50 +63,51 @@ void eliminarOcurrencias(char cad[], char caracter)
             j++;
         }
     }
+    cad[j]='\0';
 }
 
 // Funcion punto 5
 
 void concatenarCadenas(char cadena1[], char cadena2[])
 {
-    int tamanoCadena1=0;
-    for(int i=0; cadena1[i] != 0; i++)
-        tamanoCadena1++;
+    int tamanoCadena1 = tamanoCadena(cadena1);
 
-    for(int i=0; cadena2[i] != 0; i++)
+    for(int i=0; cadena2[i] != '\0'; i++)
         {
             cadena1[tamanoCadena1]=cadena2[i];
             tamanoCadena1++;
         }
+    cadena1[tamanoCadena1]='\0';
 }
 
 // Funcion punto 6
 
-void instertarCaracter(char cad[], char caracter, int pos)
+void instertarCaracter(char cad[], char caracter, int i)
 {
-    char auxAnterior=0, aux=0;
-    int estado=0;
+    char auxAnterior = '\0', aux = '\0';
+    int estado = 0;
 
-    for(int i=0; cad[i-1] != 0; i++)
+    for(i; cad[i] != '\0'; i++)
     {
         auxAnterior=aux;
-        if(i==pos-1 && estado==0)
+        if(estado==0)
         {
-            auxAnterior=cad[i];
+            aux=cad[i];
             cad[i]=caracter;
             estado=1;
 
-        }
-        if(estado==1)
-        {
+        }else{
             aux=cad[i];
             cad[i]=auxAnterior;
         }
     }
+    cad[i]=aux;
+    cad[i+1] = '\0';
 }
 
 
 int main(){
+    int tamano=0;
 
     //Punto 1 - Funciona
 
@@ -114,17 +115,19 @@ int main(){
     printf("Ingrese la cadena para calcular el tamano: ");
     fflush(stdin);
     fgets(cadena, sizeof(cadena), stdin);
-    int tamano = tamanoCadena(cadena);
-    printf("Tamano: %d\n",tamano);
+    tamano = tamanoCadena(cadena);
+    cadena[tamano-1]='\0';          //Ponemos esta linea dado que la funcion fgets toma a \n como valor previo al \0.
+                                    //De esta manera, calculamos el tamaño de una cadena y le restamos 1 para reemplazar ese \n por un \0.
+    tamano = tamanoCadena(cadena);  //Luego, calculamos su valor nuevamente con esta modificación.
+    printf("Tamano: %d\n",tamano);  //Este paso se repite en las cadenas de los otros puntos.
 
     // Punto 2 - Funciona
 
-    char cadenaNum[10];
-    int tamano2 = 10;
+    char cadenaNum[100];
     printf("Ingrese la cadena de digitos a convertir: ");
     fflush(stdin);
     fgets(cadenaNum, sizeof(cadenaNum), stdin);
-    printf("%d\n", cadenaANumero(cadenaNum, tamano2));
+    printf("%d\n", cadenaANumero(cadenaNum));
 
     // Punto 3 - Funciona
 
@@ -132,14 +135,19 @@ int main(){
     printf("Ingrese la cadena a pasar a mayusculas: ");
     fflush(stdin);
     fgets(cadenaAMayus, sizeof(cadenaAMayus), stdin);
+    tamano = tamanoCadena(cadenaAMayus);
+    cadenaAMayus[tamano-1]='\0';
     cadenaAMayusculas(cadenaAMayus);
     printf("La cadena en mayusucula es: %s\n",cadenaAMayus);
 
     // Punto 4
 
     char cadenaAModificar[100], caracterEliminar;
-    printf("Ingrese cadena\n");
-    scanf("%s",&cadenaAModificar);
+    printf("Ingrese cadena para eliminar un caracter\n");
+    fflush(stdin);
+    fgets(cadenaAModificar, sizeof(cadenaAModificar), stdin);
+    tamano = tamanoCadena(cadenaAModificar);
+    cadenaAModificar[tamano-1]='\0';
     printf("Ingrese caracter a eliminar de la cadena\n");
     fflush(stdin);
     scanf("%c",&caracterEliminar);
@@ -151,10 +159,15 @@ int main(){
     char cadena1[100];
     char cadena2[100];
     printf("Ingrese cadena 1 \n");
-    scanf("%s",&cadena1);
+    fflush(stdin);
+    fgets(cadena1, sizeof(cadena1), stdin);
+    tamano = tamanoCadena(cadena1);
+    cadena1[tamano-1]='\0';
     printf("Ingrese cadena 2 \n");
     fflush(stdin);
-    scanf("%s",&cadena2);
+    fgets(cadena2, sizeof(cadena2), stdin);
+    tamano = tamanoCadena(cadena2);
+    cadena2[tamano-1]='\0';
     concatenarCadenas(cadena1,cadena2);
     printf("Cadena concatenada: %s\n",cadena1);
 
@@ -163,7 +176,10 @@ int main(){
     char cadenaAAgregar[100], caracterInsertar;
     int pos=0;
     printf("Ingrese cadena\n");
-    scanf("%s",&cadenaAAgregar);
+    fflush(stdin);
+    fgets(cadenaAAgregar, sizeof(cadenaAAgregar), stdin);
+    tamano = tamanoCadena(cadenaAAgregar);
+    cadenaAAgregar[tamano-1]='\0';
     printf("Ingrese caracter a ingresar en la cadena\n");
     fflush(stdin);
     scanf("%c",&caracterInsertar);
@@ -179,8 +195,3 @@ int main(){
 
     return 0;
 }
-
-
-
-
-
